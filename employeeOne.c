691433ba15becc1c17f167d87350b811065bf9bb
Uziel Gaeta <ugaeta@cpp.edu>
2021-09-30 20:16:40 -0700

    implemented searhEmployeeByNumber and SearchEmployeeByName

--- employeeOne.c
+++ employeeOne.c
@@ -1,8 +1,29 @@
 #include <string.h>
-#include <stdlib.h>
 #include "employee.h"
 
-Employee EmployeeTable[] = 
-{
-    {}
+PtrToEmployee searhEmployeeByNumber(PtrToConstEmployee ptr, int tableSize, long targetNumber){
+    const PtrToConstEmployee endPtr = ptr + tableSize;
+
+    for(;ptr < endPtr; ptr++){
+        if (ptr -> number == targetNumber)
+            return (PtrToEmployee) ptr;   
+    }
+    return NULL;
 }
+
+PtrToEmployee searchEmployeeByName(PtrToConstEmployee ptr, int tableSize, char *targetname){
+    const PtrToConstEmployee endPtr = ptr + tableSize;
+    
+    for(; ptr < endPtr; ptr++){
+        if (strmcmp(ptr->name, targetName) == 0)
+            return (PtrToEmployee) ptr;
+    }
+    return NULL;
+}
+
+void main()
+{
+
+
+}
+
